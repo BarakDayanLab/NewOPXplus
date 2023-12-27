QRAMExperiment: - fix sign of FLR samples when loading playback data - fix order of plots - refactor Detectors circle presentation - relection_threshold, reflection_threshold_time, transit_condition - add to self. - method "bath_them" - do not attempt to turn to quad for writing if in playback mode - fix save_results with batcher data

Experiments/Spectrum/SpectrumExperiment.py
@@ -543,7 +543,7 @@ class SpectrumExperiment(BaseExperiment):
         # Load flouresence
         data_file = os.path.join(playback_folder, 'Flouresence.npz')
         zipped_data = np.load(data_file, allow_pickle=True)
-        self.streams['FLR_measure']['all_rows'] = zipped_data['arr_0'].tolist()
+        self.streams['FLR_measure']['all_rows'] = (-zipped_data['arr_0']).tolist()
 
         pass
 