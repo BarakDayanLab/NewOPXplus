fa49f5f7cbe5ba385811a7e4866bb1740e8c14a9
orelb <weizmann.bd@gmail.com>
2024-01-17 11:02:15 +0200

    adding new mm to pxl

--- CoolingOptimization.py
+++ CoolingOptimization.py
@@ -30,9 +30,8 @@ class CoolingSequenceOptimizer(OPX):
         self.NAvg = 3
         self.NThrow = 3
         self.imgBounds = (580, 200, 1600, 1450) # bounds to crop out of the taken pictures
-        # self.mm_to_pxl = 8.5/(830-56) # measured using ruler in focus 13/11/2022
-        # self.mm_to_pxl = 8.5/(694-34) # measured using ruler in focus 12/11/2023
-        self.mm_to_pxl = 8/(809-71) # measured using ruler in focus 29/11/2023
+        # self.mm_to_pxl = 8/(809-71) # measured using ruler in focus 29/11/2023
+        self.mm_to_pxl = 11 / (1228 - 232) #measured using ruler in focus 15/01/2024
         self.sigma_bounds = (15, 100) # This bounds sigma (x & y) of the Gaussian sigma. If value is out of bounds, fit is considered bad and not used in temp-fit
 
     def connectCamera(self):
