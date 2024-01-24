21b1504384141528a256e2148c9ff051eb1a9d0d
nadavk4 <nadav.kandel@weizmann.ac.il>
2024-01-24 13:44:40 +0200

    space to ctrl+p shortcut

--- analysis/cavity.py
+++ analysis/cavity.py
@@ -1,6 +1,7 @@
 import numpy as np
 from scipy.optimize import minimize
 from dataclasses import dataclass, field
+from typing import Optional
 
 
 @dataclass
@@ -25,19 +26,22 @@ class RubidiumLines:
 
 
 class Cavity:
-    def __init__(self, main_parameter: str, fit_parameters: list):
+    def __init__(self, main_parameter: str, fit_parameters: list, bounds):
         self.main_parameter = main_parameter
-        self.fit_parameters = fit_parameters
 
-        self.transmission_spectrum: np.ndarray = None
-        self.reflection_spectrum: np.ndarray = None
+        self.base_fit_parameters = ["x_0", "y_0", "m", "b"]
+        self.fit_parameters = fit_parameters + self.base_fit_parameters
 
-        self.x_0: float = 10
-        self.y_0: float = 0.5
-        self.m: float = 0
-        self.b: float = 0
+        self.transmission_spectrum: Optional[np.ndarray] = None
+        self.reflection_spectrum: Optional[np.ndarray] = None
 
-        self.bounds: list = []
+        self.x_0 = 1
+        self.y_0 = 0.5
+        self.m = 0
+        self.b = 0
+
+        self.base_bounds = [(-np.inf, -np.inf, -1, -np.inf), (np.inf, np.inf, 1, np.inf)]
+        self.bounds = [bounds[0] + self.base_bounds[0], bounds[1] + self.base_bounds[1]]
 
     @property
     def current_fit_value(self):
@@ -52,8 +56,11 @@ class Cavity:
         res = minimize(self.transmission_spectrum_func, np.array([self.x_0]))
         return res.x[0]
 
+    def linear_factor(self, x_detuning):
+        return self.m * x_detuning + self.b
+
     def transmission_spectrum_func(self, x_detuning):
-        pass
+        return
 
     def reflection_spectrum_func(self, x_detuning):
         pass
@@ -72,16 +79,16 @@ class Cavity:
 
 class CavityKex(Cavity):
     def __init__(self, k_i: float, h: float):
-        super().__init__("k_ex", ["k_ex", "x_0", "y_0", "m", "b"])
+        bounds = [(0, ), (np.inf, )]
+        super().__init__("k_ex", ["k_ex"], bounds)
         self.k_i = k_i
         self.h = h
         self.k_ex = 30
-        self.bounds = [(0, -np.inf, 0, -np.inf, -np.inf), (np.inf, np.inf, 1, np.inf, np.inf)]
 
     def transmission_spectrum_func(self, x_detuning):
         k_total = self.k_ex + self.k_i
         k_with_detuning = k_total + 1j * (x_detuning - self.x_0)
-        return (self.m*x_detuning+self.b)*np.abs(1 - 2 * self.k_ex * k_with_detuning / (k_with_detuning ** 2 + self.h ** 2)) ** 2 + self.y_0
+        return self.linear_factor(x_detuning) * np.abs(1 - 2 * self.k_ex * k_with_detuning / (k_with_detuning ** 2 + self.h ** 2)) ** 2 + self.y_0
 
     def reflection_spectrum_func(self, x_detuning):
         k_total = self.k_ex + self.k_i
@@ -91,12 +98,11 @@ class CavityKex(Cavity):
 
 class CavityFwhm(Cavity):
     def __init__(self):
-        super().__init__("fwhm", ["amp", "fwhm", "x_0", "y_0", "m", "b"])
+        bounds = [(-np.inf, 0), (np.inf, np.inf)]
+        super().__init__("fwhm", ["amp", "fwhm"], bounds)
         self.fwhm = 50
         self.amp = 1
 
-        self.bounds = [(-np.inf, 0, 0, 0, -np.inf, -np.inf), (np.inf, np.inf, np.inf, 1, np.inf, np.inf)]
-
     def transmission_spectrum_func(self, x_detuning):
-        return (self.m*x_detuning+self.b)*(0.5 * self.fwhm * self.amp) / (np.pi *
-                                           ((x_detuning - self.x_0) ** 2 + (0.5 * self.fwhm) ** 2)) + self.y_0
+        return self.linear_factor(x_detuning) * (0.5 * self.fwhm *
+                                                 self.amp) / (np.pi * ((x_detuning - self.x_0) ** 2 + (0.5 * self.fwhm) ** 2)) + self.y_0
