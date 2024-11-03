76f110fed8e6973b8a76b538350c225196fcc4a0
orelb <weizmann.bd@gmail.com>
2024-11-03 11:53:38 +0200

    MagneticFountain.py - 1. v_launch, v_launch_std - fix such that they would be true also for fitted alpha. 2. -v_x_launch ->v_x_launch 3. adding throiwng angle 4. GaussianFit - fixing x-y confusion 5. GaussianFit - plotting fit on original image
    MagneticFountain_Config_Table.py - 'Post_MOT_delay': 1.5

--- Experiments/MagneticFountain/MagneticFountain_Config_Table.py
+++ Experiments/MagneticFountain/MagneticFountain_Config_Table.py
@@ -9,7 +9,7 @@ Experiment_Values = {
     'Phases_Order': [Phases.MOT, Phases.FOUNTAIN, Phases.PGC, Phases.FREE_FALL, Phases.PULSE_1, Phases.INTER_PULSES, Phases.PULSE_2, Phases.POST_PULSE],
     # 'Fountain_final_Delta_freq': 0.30e6, #17.03.24
     # 'Fountain_final_Delta_freq': 0.33e6, #0.37e6
-    'Fountain_final_Delta_freq': 0.46e6,
+    'Fountain_final_Delta_freq': 0.2e6,
     'PrePulse_CH2_freq': 133.325e6, # for depump during mot, change back to 70 mhz for killing depump # Hz #Ziv Added for Cooling optimization
     'Pulse_1_CH1_Freq_f': Initial_Values['Flash_freq'],
     'Pulse_1_CH4_Freq': Initial_Values['AOM_Repump_freq_Off'],
@@ -20,20 +20,20 @@ Experiment_Values = {
 
     'Magnetic_fountain_duration': 6, # 5 [msec]
     # 'Magnetic_fountain_duration': 0,  # 5 [msec]
-    'PGC_duration': 12,  # 7  10
-    'PGC_prep_duration': 3,  # 5  8
+    'PGC_duration': 12,  # 12
+    'PGC_prep_duration': 3,  # 2
     'PGC_final_amp': 0.07, # 0.05 - till 15.01.23,         # Relative AOM amplitude between 0 to 1 - (0.12 yields 3.5 mW)
     'PGC_beams_0_off_duration': 6,
     'AOM_0_att_3rd_stage': 0,
-    'PGC_final_freq': 97.5e6, #91e6 - until 15.01.23,
+    'PGC_final_freq': 97.5e6, #97.5e6, #91e6 - until 15.01.23,
 
     #'AntiHelmholtz_delay': 0.1,  # 0.1 [msec]
-    'Post_MOT_delay': 1,
+    'Post_MOT_delay': 1.5,
 
 
     # Imaging
     'Imaging_Phase': Phases.PULSE_1,
-    'PrePulse_duration': 14,  # [msec]
+    'PrePulse_duration': 10,  # [msec]
     'Pulse_1_duration': 0.8, # 0.2,  # [msec]
 
     # 'OPX_Quad_Misalignment_Delay': 4000  # = 4us [ns]
